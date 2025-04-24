ab6b3b4a7c4edf77d1a6778622756fc119f365ca
flytiti <35782200+flytiti@users.noreply.github.com>
2025-04-24 15:57:22 +0200

    Update scenarioParser.js

--- library-multileg-multipax_133/scenarioParser.js
+++ library-multileg-multipax_133/scenarioParser.js
@@ -145,6 +145,39 @@ parseScenarioData = function(jsonData) {
 			pm.globals.set("desiredType", jsonData.scenarios[dataFileIndex].desiredType);
 			pm.globals.set("ScenarioCode", jsonData.scenarios[dataFileIndex].code);
 			validationLogger("[DEBUG] 🪲 DUMMY0")
+			
+			//*****************************************************************************************************************************************
+			// ajout TGA
+			jsonData.purchasers.some(function(purchasers){
+				if(purchasers.id==jsonData.scenarios[dataFileIndex].purchaserId){
+					var purchaserSpecs = [];
+					validationLogger('[INFO] Found purchaser informations: ');
+					if (osdmVersion == "3.4" || osdmVersion == "3.5") {
+							purchaserSpecs.push(new PurchaserContact(
+								new DetailContact(
+									purchasers.purchaserFirstName,
+									purchasers.purchaserLastName,
+									new Contact(
+										purchasers.purchaserEmail,
+										purchasers.purchaserPhoneNumber
+									)
+								)
+							));
+						} else {
+							purchaserSpecs.push(new Purchaser(
+								purchasers.purchaserReference,
+								new Detail(
+									purchasers.purchaserFirstName,
+									purchasers.purchaserLastName,
+									purchasers.purchaserEmail,
+									purchasers.purchaserPhoneNumber
+								)
+							));
+						}
+				}
+			}
+			//*****************************************************************************************************************************************
+			
 			// Loop through the passengers list to find the matching passengers list ID
 			jsonData.passengersList.some(function(passengersList){
 				if(passengersList.id==jsonData.scenarios[dataFileIndex].passengersListId){
@@ -152,7 +185,10 @@ parseScenarioData = function(jsonData) {
 					pm.globals.set(OFFER.PASSENGER_NUMBER, passengersList.passengers.length);
 					var offerPassengerSpecs = [];
 					var passengerSpecs = [];
-					var purchaserSpecs = [];
+					//*****************************************************************************************************************************************
+					// suppression TGA
+					//var purchaserSpecs = [];
+					//*****************************************************************************************************************************************
 					var passengerReferences = [];
 					var passengerAdditionalData = [];
 					var passengerIndex = 0;
@@ -162,14 +198,17 @@ parseScenarioData = function(jsonData) {
 						pm.globals.set(passengerKey, uuid.v4());
 						offerPassengerSpecs.push(new AnonymousPassengerSpec(
 							//pm.globals.get(passengerKey),
-							passenger.reference,
-							passenger.type,
-							passenger.dateOfBirth,
+							passenger.passengerReference,
+							passenger.passengerType,
+							passenger.passengerDateOfBirth,
 						));
 						validationLogger("[DEBUG] 🪲 DUMMY1")
 
 						var osdmVersion = pm.globals.get("osdmVersion");
-						if (osdmVersion == "3.4" || osdmVersion == "3.5") {
+
+						//*****************************************************************************************************************************************
+						// suppression TGA
+						/*if (osdmVersion == "3.4" || osdmVersion == "3.5") {
 							purchaserSpecs.push(new PurchaserContact(
 								new DetailContact(
 									passenger.purchaserFirstName,
@@ -189,35 +228,36 @@ parseScenarioData = function(jsonData) {
 									passenger.phoneNumber
 								)
 							));
-						}
+						}*/
+						//*****************************************************************************************************************************************
 
 						validationLogger("[DEBUG] 🪲 DUMMY2")
 						if (osdmVersion == "3.5") {
 							passengerSpecs.push(new PassengerSpec(
 								//pm.globals.get(passengerKey),
-								passenger.reference,
-								passenger.type,
-								passenger.dateOfBirth,
+								passenger.passengerReference,
+								passenger.passengerType,
+								passenger.passengerDateOfBirth,
 								new DetailContact(
-									passenger.firstName,
-									passenger.lastName,
+									passenger.passengerFirstName,
+									passenger.passengerLastName,
 									new Contact(
-										passenger.email || null,
-										passenger.phoneNumber || null
+										passenger.passengerEmail || null,
+										passenger.passengerPhoneNumber || null
 									)
 								)
 							));
 						} else {							
 							passengerSpecs.push(new PassengerSpec(
 								//pm.globals.get(passengerKey),
-								passenger.reference,
-								passenger.type,
-								passenger.dateOfBirth,
+								passenger.passengerReference,
+								passenger.passengerType,
+								passenger.passengerDateOfBirth,
 								new Detail(
-									passenger.firstName,
-									passenger.lastName,
-									passenger.email || null,
-									passenger.phoneNumber || null
+									passenger.passengerFirstName,
+									passenger.passengerLastName,
+									passenger.passengerEmail || null,
+									passenger.passengerPhoneNumber || null
 								)
 							));
 						}
@@ -226,11 +266,11 @@ parseScenarioData = function(jsonData) {
 						//passengerReferences.push(pm.globals.get(passengerKey));
 
 						let passengerAdditionalDataStruct = {
-							updateFirstName: passenger.updateFirstName,
-							updateLastName: passenger.updateLastName,
-							updateDateOfBirth: passenger.updateDateOfBirth,
-							updateEmail: passenger.updateEmail,
-							updatePhoneNumber: passenger.updatePhoneNumber,
+							updateFirstName: passenger.updatePassengerFirstName,
+							updateLastName: passenger.updatePassengerLastName,
+							updateDateOfBirth: passenger.updatePassengerDateOfBirth,
+							updateEmail: passenger.updatePassengerEmail,
+							updatePhoneNumber: passenger.updatePassengerPhoneNumber,
 						};
 						passengerAdditionalData.push(passengerAdditionalDataStruct);
 						passengerIndex++;
